1fa18d6991a37ed35b419d86c430f2e63fb764bd
mohammadjavad <mjkmo123@gmail.com>
2022-08-16 12:36:14 +0430

    add user table

--- bakery/migrator/src/m20220816_111225_create_user.rs
+++ bakery/migrator/src/m20220816_111225_create_user.rs
@@ -6,43 +6,47 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
 
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(User::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(Post::Id)
+                        ColumnDef::new(User::Id)
                             .integer()
                             .not_null()
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .col(ColumnDef::new(User::Name).string().not_null())
+                    .col(ColumnDef::new(User::Icon).string().not_null())
+                    .col(ColumnDef::new(User::Username).string().not_null().unique_key())
+                    .col(ColumnDef::new(User::Password).string().not_null())
+                    .col(ColumnDef::new(User::IsVerified).boolean().not_null().default(false))
+                    .col(ColumnDef::new(User::CreatedAt).date_time().not_null().extra("DEFAULT NOW()".into()))
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
 
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(User::Table).to_owned())
             .await
     }
 }
 
 /// Learn more at https://docs.rs/sea-query#iden
 #[derive(Iden)]
-enum Post {
+enum User {
     Table,
     Id,
-    Title,
-    Text,
+    Name,
+    Icon,
+    Username,
+    Password,
+    CreatedAt,
+    IsVerified,
 }
