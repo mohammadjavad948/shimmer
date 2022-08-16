149600760e0e3e06221c565e21bac4c08cf9f440
mohammadjavad <mjkmo123@gmail.com>
2022-08-16 17:48:54 +0430

    cards in pocket

--- bakery/migrator/src/lib.rs
+++ bakery/migrator/src/lib.rs
@@ -3,6 +3,7 @@ pub use sea_orm_migration::prelude::*;
 mod m20220816_111225_create_user;
 mod m20220816_123945_create_card_group;
 mod m20220816_124525_create_card;
+mod m20220816_174157_create_cards_in_pocket;
 
 pub struct Migrator;
 
@@ -13,6 +14,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20220816_111225_create_user::Migration),
             Box::new(m20220816_123945_create_card_group::Migration),
             Box::new(m20220816_124525_create_card::Migration),
+            Box::new(m20220816_174157_create_cards_in_pocket::Migration),
         ]
     }
 }
---
+++ bakery/migrator/src/m20220816_174157_create_cards_in_pocket.rs
@@ -0,0 +1,50 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(CardsInPocket::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(CardsInPocket::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(CardsInPocket::CardId).integer().not_null())
+                    .col(ColumnDef::new(CardsInPocket::UserId).integer().not_null())
+                    .col(ColumnDef::new(CardsInPocket::StartDate).date_time().not_null())
+                    .col(ColumnDef::new(CardsInPocket::Level).integer().not_null().default(1))
+                    .col(ColumnDef::new(CardsInPocket::CreatedAt).date_time().not_null().extra("DEFAULT NOW()".into()))
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+
+        manager
+            .drop_table(Table::drop().table(CardsInPocket::Table).to_owned())
+            .await
+    }
+}
+
+/// Learn more at https://docs.rs/sea-query#iden
+#[derive(Iden)]
+enum CardsInPocket {
+    Table,
+    Id,
+    CardId,
+    UserId,
+    StartDate,
+    Level,
+    CreatedAt
+}
