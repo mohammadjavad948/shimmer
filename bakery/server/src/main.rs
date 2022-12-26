12c584e3ef1d8b4b572cfe491977c77f4f4374c5
mohammadjavad <mjkmo123@gmail.com>
2022-12-26 21:35:42 +0330

    tracing

--- bakery/server/src/main.rs
+++ bakery/server/src/main.rs
@@ -2,7 +2,8 @@ use axum::{Extension, Router};
 use database::sea_orm::{Database, DatabaseConnection};
 use router::routes;
 use state::State;
-use std::sync::Arc;
+use std::{net::SocketAddr, sync::Arc};
+use tracing::Level;
 
 mod actions;
 mod helpers;
@@ -12,15 +13,26 @@ mod state;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(Level::TRACE)
+        .compact()
+        .init();
+
     let db_url = std::env::var("DATABASE_URL").unwrap();
     let db: DatabaseConnection = Database::connect(db_url).await.unwrap();
 
     let state = Arc::new(State { db });
 
     // build our application with a single route
-    let app = Router::new().nest("/", routes()).layer(Extension(state));
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    let app = Router::new()
+        .nest("/", routes())
+        .layer(Extension(state))
+        .layer(tower_http::trace::TraceLayer::new_for_http());
+
+    // run it with hyper on localhost:5000
+    let addr = SocketAddr::from(([0, 0, 0, 0], 5000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
