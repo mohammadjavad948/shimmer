a4d9a6e90a2a6bd9209c15ab49dfa32e595f5392
mohammadjavad <mjkmo123@gmail.com>
2022-12-29 19:40:14 +0330

    init front

---
+++ front/src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <div className="flex">
+      <div className="flex-1">
+        <span className="font-bold text-3xl">
+          hello
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default App
