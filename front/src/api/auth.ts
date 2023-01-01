9b56f5b0edffc4f7974f4867cd09ff72c040221c
mohammadjavad <mjkmo123@gmail.com>
2023-01-01 14:44:38 +0330

    api

---
+++ front/src/api/auth.ts
@@ -0,0 +1,9 @@
+import { api } from "./api";
+
+export function login(body: any){
+    return api.post("/auth/login", body);
+}
+
+export function signup(body: any){
+    return api.post("/auth/signup", body);
+}
