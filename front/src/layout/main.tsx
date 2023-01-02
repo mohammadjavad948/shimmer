7642ea7eb93b9dfe1f066067819fca0d6ed91598
mohammadjavad <mjkmo123@gmail.com>
2023-01-02 12:28:35 +0330

    layout

--- front/src/layout/main.tsx
+++ front/src/layout/main.tsx
@@ -2,9 +2,11 @@ import Aside from '../components/aside';
 
 export function MainLayout({ children }: { children: any }) {
   return (
-    <div className="flex h-[100vh] w-[100%] flex-col bg-gray-100 md:min-h-[100vh] md:flex-row md:gap-2">
-      <Aside />
-      <div className="flex-1 bg-gray-200 md:min-h-[100vh]">hi</div>
+    <div className="flex h-screen w-screen bg-gray-100">
+      {/* <Aside /> */}
+      <div className="flex-1 container p-5 mx-auto bg-gray-200">
+        {children}
+      </div>
     </div>
   );
 }
