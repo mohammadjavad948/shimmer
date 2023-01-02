9a85499de5d3266f4377af48ca1d7224ea723f08
mohammadjavad <mjkmo123@gmail.com>
2023-01-02 17:01:46 +0330

    card group

--- front/src/pages/card_group/AllCardGroup.tsx
+++ front/src/pages/card_group/AllCardGroup.tsx
@@ -2,16 +2,16 @@ import { MainLayout } from "../../layout/main";
 import {AiOutlinePlus} from 'react-icons/ai';
 import { useQuery } from "@tanstack/react-query";
 import { allCardGroup } from "../../api/card_group";
+import {BiLockAlt, BiShow} from "react-icons/bi";
 
 export default function AllCardGroup(){
 
     const {data, isLoading} = useQuery(['card-group'], allCardGroup);
 
-    console.log(data);
     return (
         <MainLayout>
             <div
-                className="w-full flex justify-between"
+                className="w-full flex justify-between mb-2 items-center"
             >
                 <span className="text-slate-600 font-semibold text-base">
                     Card Groups
@@ -23,8 +23,35 @@ export default function AllCardGroup(){
                     New
                     <AiOutlinePlus size={18}/>
                 </button>
+            </div>
+            <div className="w-full grid grid-cols-4 gap-3">
                 {!isLoading && (
-                    <div></div>
+                    data?.data.map((el, i) => {
+                        return (
+                            <div
+                                className="bg-white p-4 rounded-lg hover:shadow-md cursor-pointer"
+                                key={i}
+                            >
+                                <span
+                                    className="text-lg font-bold flex gap-2 items-center"
+                                >
+                                    {el.name}
+
+                                    {el.is_public && (
+                                        <span className="p-1 bg-gray-200 text-gray-700 rounded-lg">
+                                            <BiShow />
+                                        </span>
+                                    )}
+
+                                    {!el.is_public && (
+                                        <span className="p-1 bg-gray-200 text-gray-700 rounded-lg">
+                                            <BiLockAlt />
+                                        </span>
+                                    )}
+                                </span>
+                            </div>
+                        )
+                    })
                 )}
             </div>
         </MainLayout>
