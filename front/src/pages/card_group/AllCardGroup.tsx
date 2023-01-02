de2a95385c7391c66ac5460c580470c9d92753e5
mohammadjavad <mjkmo123@gmail.com>
2023-01-02 17:02:29 +0330

    prettier

--- front/src/pages/card_group/AllCardGroup.tsx
+++ front/src/pages/card_group/AllCardGroup.tsx
@@ -1,59 +1,51 @@
-import { MainLayout } from "../../layout/main";
-import {AiOutlinePlus} from 'react-icons/ai';
-import { useQuery } from "@tanstack/react-query";
-import { allCardGroup } from "../../api/card_group";
-import {BiLockAlt, BiShow} from "react-icons/bi";
+import { MainLayout } from '../../layout/main';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { useQuery } from '@tanstack/react-query';
+import { allCardGroup } from '../../api/card_group';
+import { BiLockAlt, BiShow } from 'react-icons/bi';
 
-export default function AllCardGroup(){
+export default function AllCardGroup() {
+  const { data, isLoading } = useQuery(['card-group'], allCardGroup);
 
-    const {data, isLoading} = useQuery(['card-group'], allCardGroup);
+  return (
+    <MainLayout>
+      <div className="mb-2 flex w-full items-center justify-between">
+        <span className="text-base font-semibold text-slate-600">
+          Card Groups
+        </span>
 
-    return (
-        <MainLayout>
-            <div
-                className="w-full flex justify-between mb-2 items-center"
-            >
-                <span className="text-slate-600 font-semibold text-base">
-                    Card Groups
+        <button className="flex items-center gap-2 rounded-md bg-slate-300 py-2 px-4 font-semibold text-slate-800 hover:bg-slate-400">
+          New
+          <AiOutlinePlus size={18} />
+        </button>
+      </div>
+      <div className="grid w-full grid-cols-4 gap-3">
+        {!isLoading &&
+          data?.data.map((el, i) => {
+            return (
+              <div
+                className="cursor-pointer rounded-lg bg-white p-4 hover:shadow-md"
+                key={i}
+              >
+                <span className="flex items-center gap-2 text-lg font-bold">
+                  {el.name}
+
+                  {el.is_public && (
+                    <span className="rounded-lg bg-gray-200 p-1 text-gray-700">
+                      <BiShow />
+                    </span>
+                  )}
+
+                  {!el.is_public && (
+                    <span className="rounded-lg bg-gray-200 p-1 text-gray-700">
+                      <BiLockAlt />
+                    </span>
+                  )}
                 </span>
-
-                <button
-                    className="flex gap-2 py-2 px-4 hover:bg-slate-400 font-semibold rounded-md bg-slate-300 text-slate-800 items-center"
-                >
-                    New
-                    <AiOutlinePlus size={18}/>
-                </button>
-            </div>
-            <div className="w-full grid grid-cols-4 gap-3">
-                {!isLoading && (
-                    data?.data.map((el, i) => {
-                        return (
-                            <div
-                                className="bg-white p-4 rounded-lg hover:shadow-md cursor-pointer"
-                                key={i}
-                            >
-                                <span
-                                    className="text-lg font-bold flex gap-2 items-center"
-                                >
-                                    {el.name}
-
-                                    {el.is_public && (
-                                        <span className="p-1 bg-gray-200 text-gray-700 rounded-lg">
-                                            <BiShow />
-                                        </span>
-                                    )}
-
-                                    {!el.is_public && (
-                                        <span className="p-1 bg-gray-200 text-gray-700 rounded-lg">
-                                            <BiLockAlt />
-                                        </span>
-                                    )}
-                                </span>
-                            </div>
-                        )
-                    })
-                )}
-            </div>
-        </MainLayout>
-    )
+              </div>
+            );
+          })}
+      </div>
+    </MainLayout>
+  );
 }
