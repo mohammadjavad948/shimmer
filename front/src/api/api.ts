d6eabde77ba0ecfb1cfbf0bf05e17c4505487596
mohammadjavad <mjkmo123@gmail.com>
2023-01-02 15:19:51 +0330

    auth token header

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -1,5 +1,12 @@
 import axios from 'axios';
+import useTokenStore from '../store/tokenStore';
 
 export const api = axios.create({
   baseURL: 'http://localhost:5000',
 });
+
+export function authHeader(){
+  return {
+    auth: useTokenStore.getState().token
+  }
+}
