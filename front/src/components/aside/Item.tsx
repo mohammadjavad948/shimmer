61fe9646639e1bdb339b414e4c794d3d08cee767
mohammadjavad <mjkmo123@gmail.com>
2023-01-03 20:00:28 +0330

    nice

--- front/src/components/aside/Item.tsx
+++ front/src/components/aside/Item.tsx
@@ -1,6 +1,6 @@
 export function AsideItem(props: { children: any }) {
   return (
-    <div className="flex aspect-square h-[100%] cursor-pointer items-center justify-center rounded-md md:h-auto md:w-[100%]">
+    <div className="flex h-[100%] py-3 px-4 rounded-lg bg-slate-400 cursor-pointer items-center md:h-auto md:w-[100%]">
       {props.children}
     </div>
   );
