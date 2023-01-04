2155a09cd07b16b6baba6713946a3556f27ef296
mohammadjavad <mjkmo123@gmail.com>
2023-01-04 08:46:01 +0330

    colors

--- front/src/components/aside/Item.tsx
+++ front/src/components/aside/Item.tsx
@@ -1,11 +1,12 @@
 export function AsideItem(props: { children: any, icon: any }) {
   return (
     <div 
-      className="flex py-3 px-4 rounded-lg cursor-pointer items-center md:gap-2 md:h-auto md:w-[100%]"
+      className="flex py-3 px-4 rounded-lg cursor-pointer text-slate-800 items-center md:gap-2 md:h-auto md:w-[100%] hover:bg-slate-300"
     >
-      <span className="hidden md:block text-gray-800">
+      <span className="hidden md:block">
         {props.icon}
       </span>
+      
       {props.children}
     </div>
   );
