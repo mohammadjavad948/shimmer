68e9ec1f0c55ad64e5e2aa135e9fb25762442264
mohammadjavad <mjkmo123@gmail.com>
2023-01-04 12:53:46 +0330

    fix

--- front/src/components/topbar/index.tsx
+++ front/src/components/topbar/index.tsx
@@ -1,6 +1,6 @@
 export default function TopBar() {
   return (
-    <div className="flex h-10 w-full items-center border-b-2 border-b-slate-400 px-2">
+    <div className="hidden md:flex h-10 w-full items-center border-b-2 border-b-slate-400 px-2">
       <div className="flex gap-2">
         <span className="cursor-pointer rounded-md py-1 px-3 font-bold text-slate-800 hover:bg-slate-300">
           &lt;
