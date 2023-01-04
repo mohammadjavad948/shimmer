4115f64149100ffc357a7dc434d9e96401f64a8d
mohammadjavad <mjkmo123@gmail.com>
2023-01-04 09:03:16 +0330

    fix

--- front/src/components/aside/Item.tsx
+++ front/src/components/aside/Item.tsx
@@ -1,6 +1,6 @@
 export function AsideItem(props: { children: any; icon: any }) {
   return (
-    <div className="flex cursor-pointer items-center rounded-lg py-3 px-4 text-slate-800 hover:bg-slate-300 md:h-auto md:w-[100%] md:gap-2">
+    <div className="flex cursor-pointer items-center rounded-lg py-3 px-4 font-semibold text-slate-800 hover:bg-slate-300 md:h-auto md:w-[100%] md:gap-2">
       <span className="hidden md:block">{props.icon}</span>
       {props.children}
     </div>
