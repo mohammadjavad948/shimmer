bf5a1314d206c643f7021d7f492856771fe5aa03
mohammadjavad <mjkmo123@gmail.com>
2023-01-05 11:01:08 +0330

    icon

--- front/src/components/form/Answers.tsx
+++ front/src/components/form/Answers.tsx
@@ -1,4 +1,6 @@
 import { Field, FieldProps } from "formik";
+import { AiOutlinePlus } from 'react-icons/ai';
+
 
 export function AnswerForm(props: {name: string}){
 
@@ -17,13 +19,13 @@ function Answers(props: {data: FieldProps}){
 
     return (
         <div className="w-full flex flex-col">
-            <div className="flex justify-between">
+            <div className="flex justify-between items-center">
                 <span className="text-sm text-slate-500">
                     Answers
                 </span>
 
-                <button className="px-3 py-1 rounded-lg font-bold bg-slate-300">
-                    +
+                <button className="px-2 py-2 rounded-lg font-bold bg-slate-300">
+                    <AiOutlinePlus size={18} />
                 </button>
             </div>
         </div>
